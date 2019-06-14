5a47d2dca3723b355599a5105d8036f59fa731f1
Erik Sommer <ersotech@posteo.de>
2019-06-14 19:17:57 +0200

    base stands ... npm and vs code works

--- vagrant/scripts/pre_provision.sh
+++ vagrant/scripts/pre_provision.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+set -x
 apt-get update
 apt-get install -y python-pip libssl-dev sudo
 pip install ansible
 su vagrant -c "mkdir -p /home/vagrant/Desktop"
+ansible-galaxy install --roles-path /etc/ansible/roles -r /vagrant/requirements.yml
