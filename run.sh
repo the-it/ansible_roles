d9abf85f5ac806a487451f6c974a7fdc8fe11a84
Erik Sommer <ersotech@posteo.de>
2020-07-15 14:48:04 +0200

    repair python installation

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ansible-galaxy install -r requirements.yml
-ansible-playbook -e 'ansible_python_interpreter=/usr/bin/python3' -K -vv $1 $2 $3 $4 $5 $6 $7 $8 $9
+ansible-playbook -e 'ansible_python_interpreter=/usr/bin/python3' -K -v $1 $2 $3 $4 $5 $6 $7 $8 $9
